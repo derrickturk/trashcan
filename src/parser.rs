b3bb6dd6cd4aa3703a29f7147375ce27da4452fa
derrickturk <dwt@terminusdatascience.com>
2017-02-22 22:09:32 -0600

    Start parsing stuff.

--- src/parser.rs
+++ src/parser.rs
@@ -29,8 +29,36 @@ named!(module(&[u8]) -> Module, ws!(do_parse!(
 
 */
 
+named!(assign_op(&[u8]) -> AssignOp, alt_complete!(
+    map!(char!('='), |_| AssignOp::Assign)
+  | map!(tag!("+="), |_| AssignOp::AddAssign)
+  | map!(tag!("-="), |_| AssignOp::SubAssign)
+  | map!(tag!("*="), |_| AssignOp::MulAssign)
+  | map!(tag!("/="), |_| AssignOp::DivAssign)
+  | map!(tag!("%="), |_| AssignOp::ModAssign)
+  | map!(tag!("^="), |_| AssignOp::PowAssign)
+  | map!(tag!("@="), |_| AssignOp::StrCatAssign)
+  | map!(tag!("&="), |_| AssignOp::BitAndAssign)
+  | map!(tag!("|="), |_| AssignOp::BitOrAssign)
+  | map!(tag!("&&="), |_| AssignOp::LogAndAssign)
+  | map!(tag!("||="), |_| AssignOp::LogOrAssign)
+));
+
+named!(un_op(&[u8]) -> UnOp, map!(one_of!("-~!"), |c| match c {
+    '-' => UnOp::Negate,
+    '~' => UnOp::BitNot,
+    '!' => UnOp::LogNot,
+    _ => panic!("internal parser error")
+}));
+
 named!(bin_op(&[u8]) -> BinOp, alt_complete!(
-    map!(one_of!("+-*/%^@<>&|"), |c| match c {
+    map!(tag!("=="), |_| BinOp::Eq)
+  | map!(tag!("!="), |_| BinOp::NotEq)
+  | map!(tag!("<="), |_| BinOp::LtEq)
+  | map!(tag!(">="), |_| BinOp::GtEq)
+  | map!(tag!("&&"), |_| BinOp::LogAnd)
+  | map!(tag!("||"), |_| BinOp::LogOr)
+  | map!(one_of!("+-*/%^@<>&|"), |c| match c {
         '+' => BinOp::Add,
         '-' => BinOp::Sub,
         '*' => BinOp::Mul,
@@ -52,10 +80,48 @@ mod test {
     use nom::IResult;
 
     #[test]
-    fn parse_bin_op() {
+    fn parse_assign_ops() {
+        if let IResult::Done(_, AssignOp::Assign) = assign_op("=".as_bytes()) {
+        } else {
+            panic!("didn't parse AssignOp::Assign");
+        }
+
+        if let IResult::Done(_, AssignOp::BitAndAssign) = assign_op("&=".as_bytes()) {
+        } else {
+            panic!("didn't parse AssignOp::BitAndAssign");
+        }
+
+        assert!(assign_op("xx=".as_bytes()).is_err());
+    }
+
+    #[test]
+    fn parse_un_ops() {
+        if let IResult::Done(_, UnOp::Negate) = un_op("-".as_bytes()) {
+        } else {
+            panic!("didn't parse UnOp::Negate");
+        }
+
+        assert!(un_op("x".as_bytes()).is_err());
+    }
+
+    #[test]
+    fn parse_bin_ops() {
         if let IResult::Done(_, BinOp::Add) = bin_op("+".as_bytes()) {
         } else {
             panic!("didn't parse BinOp::Add");
         }
+
+        if let IResult::Done(_, BinOp::BitAnd) = bin_op("&".as_bytes()) {
+        } else {
+            panic!("didn't parse BinOp::BitAnd");
+        }
+
+        if let IResult::Done(_, BinOp::LogAnd) = bin_op("&&".as_bytes()) {
+        } else {
+            panic!("didn't parse BinOp::LogAnd");
+        }
+
+        assert!(bin_op("xx".as_bytes()).is_err());
     }
+
 }
