f6d43a2058be59dcdcd0e2c95e9803ecec9bf7e7
derrickturk <dwt@terminusdatascience.com>
2017-03-06 20:05:40 -0600

    Gensym forloop variables.

--- src/analysis/transforms.rs
+++ src/analysis/transforms.rs
@@ -7,7 +7,26 @@ use ast::*;
 pub fn for_loop_var_gensym(dumpster: Dumpster) -> Dumpster {
     Dumpster {
         modules: dumpster.modules.into_iter().map(|m| {
-            m
+            Module {
+                name: m.name,
+                data: match m.data {
+                    ModuleKind::Normal(items) =>
+                        ModuleKind::Normal(items.into_iter().map(|i| {
+                            match i {
+                                NormalItem::Function(def) =>
+                                    NormalItem::Function(FunDef {
+                                        name: def.name,
+                                        access: def.access,
+                                        params: def.params,
+                                        ret: def.ret,
+                                        body: for_loop_var_gensym_stmts(def.body),
+                                        loc: def.loc,
+                                    }),
+                            }
+                        }).collect()),
+                },
+                loc: m.loc,
+            }
         }).collect()
     }
 }
@@ -20,4 +39,20 @@ pub fn short_circuit_logicals(dumpster: Dumpster) -> Dumpster {
             m
         }).collect()
     }
+}
+
+fn for_loop_var_gensym_stmts(stmts: Vec<Stmt>) -> Vec<Stmt> {
+    stmts.into_iter().map(|s| {
+        let sub_var = match s.data {
+            StmtKind::ForLoop { ref var, .. } => Some(var.0.clone()),
+            _ => None,
+        };
+
+        if let Some(var) = sub_var {
+            let newvar = gensym();
+            s.substitute(&var, &newvar)
+        } else {
+            s
+        }
+    }).collect()
 }
